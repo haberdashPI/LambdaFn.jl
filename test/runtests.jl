792933f2d2638b9f6ec80c1c5dd6d7b45cf02bff
David Little <david.frank.little@gmail.com>
2019-06-13 13:48:29 -0400

    revised versions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using LambdaFn
 using Test
 
-@testset "LambdaFn.jl" begin
+@testset "LambdaFn" begin
     @test @λ(_ + _)(1,2) == 3
     @test @λ(_a + _b*_a)(3,2) == 9
     @test_throws Exception @eval(@λ(_ + _1))
