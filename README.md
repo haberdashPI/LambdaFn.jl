0a037a143cf5009e90a5fcb2ebc220cd82cfa9ce
David Little <david.frank.little@gmail.com>
2019-06-13 13:38:59 -0400

    initial commit

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 [![Build Status](https://travis-ci.com/haberdashPI/LambdaFn.jl.svg?branch=master)](https://travis-ci.com/haberdashPI/LambdaFn.jl)
 [![Build Status](https://ci.appveyor.com/api/projects/status/github/haberdashPI/LambdaFn.jl?svg=true)](https://ci.appveyor.com/project/haberdashPI/LambdaFn-jl)
 [![Codecov](https://codecov.io/gh/haberdashPI/LambdaFn.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/haberdashPI/LambdaFn.jl)
+
+This small package provides an alternative syntax for anonymous functions,
+using `_` as a placeholder for each argument OR variables prefixed with `_` to
+indicate the arguments. For example:
+
+```julia
+using LambdaFn
+
+@λ(_ + _) # == x -> x+x
+@λ(_a*_b + _a) # == (a,b) -> a*b + a
+filter(@λ(_.value > 10),data) # == filter(x -> x.value > 10,data)
+```
+
+This syntax resembles the syntax in [this
+proposal](https://github.com/JuliaLang/julia/pull/24990), and would be
+replaced by such a feature. This package is simply a convienient way to use
+such an underscore syntax in cases where it is more readable than the default
+syntax. (Note however that it can take just as many, or more characters as the
+standard anonymous function syntax).
--- src/LambdaFn.jl
+++ src/LambdaFn.jl
@@ -1,5 +1,50 @@
 module LambdaFn
+using MacroTools: postwalk
+using Printf
 
-greet() = print("Hello World!")
+macro λ(body)
+    args = Vector{Symbol}()
+    implicit::Union{Missing,Bool} = missing
+
+    body = postwalk(body) do expr
+        if expr isa Symbol
+            sym = string(expr)
+            if sym == "_"
+                if ismissing(implicit)
+                    implicit = true
+                elseif !implicit
+                    error("Cannot use both implicit (_) and explicit (_1) "*
+                          "arguments in the same @λ body.")
+                end
+
+                expr = gensym("_",string(length(args)+1))
+                push!(args,expr)
+                expr
+            elseif startswith(sym,"_")
+                if ismissing(implicit)
+                    implicit = false
+                elseif implicit
+                    error("Cannot use both implicit (_) and explicit (_1) "*
+                          "arguments in the same @λ body.")
+                end
+
+                if expr ∉ args
+                    push!(args,expr)
+                end
+                expr
+            else
+                expr
+            end
+        else
+            expr
+        end
+    end
+
+    quote
+        function lambda($(map(esc,args)...))
+            $(esc(body))
+        end
+    end
+end
 
 end # module
